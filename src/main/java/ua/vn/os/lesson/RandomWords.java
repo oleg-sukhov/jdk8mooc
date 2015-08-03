9b09e31614b516296b58277d22ff99c0e7b3e2ff
oleg-sukhov <oleg.sukhov@email.ua>
2015-08-03 23:13:57 +0300

    JDK8MOOC: Lesson3 HomeWork

---
+++ src/main/java/ua/vn/os/lesson/RandomWords.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright © 2014, Oracle and/or its affiliates. All rights reserved.
+ * <p>
+ * JDK 8 MOOC Lesson 3 homework
+ */
+package ua.vn.os.lesson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Class to generate a list of random words
+ *
+ * @author Simon Ritter (@speakjava)
+ */
+public class RandomWords {
+    private final List<String> sourceWords;
+
+    /**
+     * Constructor
+     *
+     * @throws IOException If the source words file cannot be read
+     */
+    public RandomWords() throws IOException, URISyntaxException {
+        try (BufferedReader reader = Files.newBufferedReader(
+                Paths.get(this.getClass().getResource("words.txt").toURI()), StandardCharsets.UTF_8)) {
+            sourceWords = reader.lines().collect(Collectors.toList());
+            System.out.println("Loaded " + sourceWords.size() + " words");
+        }
+    }
+
+    /**
+     * Create a list of a given size containing random words
+     *
+     * @param listSize The size of the list to create
+     * @return The created list
+     */
+    public List<String> createList(int listSize) {
+        Random rand = new Random();
+        return rand
+                .ints(listSize, 0, 235885)
+                .mapToObj(sourceWords::get)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Return the list of all source words, which cannot be modified
+     *
+     * @return The unmodifiable list of all source words
+     */
+    public List<String> allWords() {
+        return Collections.unmodifiableList(sourceWords);
+    }
+}
